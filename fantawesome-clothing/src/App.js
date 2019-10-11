ff18c477a9cc46e807a3172583a8f47a2d83b7d6
fhernan22 <fhern103@fiu.edu>
2019-10-11 00:36:46 -0400

    Finishing sign in handleSubmit function

--- fantawesome-clothing/src/App.js
+++ fantawesome-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage  from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
