7adb14b62e2cc04cb0ca6cc14c216607400ac93c
fhernan22 <fhern103@fiu.edu>
2019-12-17 20:11:11 -0500

    Added cart icon and cart dropdown

--- fantawesome-clothing/src/components/header/header.component.jsx
+++ fantawesome-clothing/src/components/header/header.component.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import './header.styles.scss'
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <h1 className='logo'>FantAwesome</h1>
@@ -26,12 +28,15 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
+        {hidden ? null : <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
